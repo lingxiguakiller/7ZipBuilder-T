6379eada75118a5428d5f8a8153d756ed7cbd02e
lingxigua <123914305+lingxiguakiller@users.noreply.github.com>
2025-07-21 17:27:04 +0800

    Update SubPrepare.ps1
    Fix: 图标路径

--- SubPrepare.ps1
+++ SubPrepare.ps1
@@ -5,4 +5,5 @@ param(
     [string] $BuildVersion
 )
 
-Copy-Item -Force -Recurse -Path "FileIcons\*.ico" -Destination "$BuildDirectory\CPP\7zip\Archive\Icons"
+Copy-Item -Force -Recurse -Path "Resources\FileIcons\*.ico" -Destination "$BuildDirectory\CPP\7zip\Archive\Icons"
+Copy-Item -Force -Recurse -Path "Resources\ToolBarIcons\*.bmp" -Destination "$BuildDirectory\CPP\7zip\UI\FileManager"
